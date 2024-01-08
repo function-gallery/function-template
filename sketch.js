6ebdd6c6fc4d156be7044522a97bc34ea2edb500
George Gally <GeorgeGally@users.noreply.github.com>
2024-01-08 20:59:24 +0800

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+/* Your sketch goes here */
+/* Math.random() -> mathRand() */
+/* M = Math, W = Window, D = Document */
+
+function setup(){
+  
+}
+
+function draw(){
+
+}
+
+window.$functionAttribute = {
+  "trait1": mathRand() * 10,
+  "trait2": mathRand() * 100,
+  "trait3": mathRand() * 1000
+}
+
+console.log(window.$functionAttribute)
