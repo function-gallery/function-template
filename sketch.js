2dbfccf3a452170a366e43bbae86ea96f2ef67bc
George Gally <GeorgeGally@users.noreply.github.com>
2024-02-20 15:44:12 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,26 @@
 
 /* Your sketch goes here */
-/* Math.random() -> mathRand() */
-/* M = Math, W = Window, D = Document */
+/* Math.random() -> mathRand() = R.D() */
+/* RI() -> random Integer */
+/* R.I() -> random Integer */
+/* Globals: M = Math, W = Window, D = Document */
+
 
 function setup(){
   createCanvas(windowWidth, windowHeight)
   noStroke()
+  window.$functionAttribute = {
+    "trait1": mathRand() * 10,
+    "trait2": mathRand() * 100,
+    "trait3": mathRand() * 1000
+  }
+  frameRate(120)
+  console.log(window.$functionAttribute)
 }
 
 function draw(){
-  background(220, .1)
-  fill(255, 0, 0)
-  ellipse(RI(0, width), RI(0, height), 20, 20)
-  console.log(FN.N(0, 1));
+  background(220, .08)
+  fill(RI(255), RI(255), 0)
+  ellipse(RI(width), R.I(height), 40, 40)
+  //console.log(FN.N(0, 1));
 }
-
-window.$functionAttribute = {
-  "trait1": mathRand() * 10,
-  "trait2": mathRand() * 100,
-  "trait3": mathRand() * 1000
-}
-
-console.log(window.$functionAttribute)
