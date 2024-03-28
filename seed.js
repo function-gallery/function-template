c2a111eee2df603a84cdfd16980e38967b8fe459
George Gally <GeorgeGally@users.noreply.github.com>
2024-03-28 16:43:01 +0800

    Update seed.js

--- seed.js
+++ seed.js
@@ -1,4 +1,7 @@
-// use ?dev=xx in your params if developing locally !!
+// use ?dev=1 in your params if developing locally !!
+// this will add the full reference to ordinals.com
+
+// use static=xxxxx to
 
 let M = Math,
   D = document,
@@ -8,11 +11,70 @@ let M = Math,
 let seed, dna
 let blockHash = ""
 
+let ord = 'https://ordinals.com'
+let con = '/content/'
 let dev = new URLSearchParams(W.location.search).get('dev')
+let U = dev ? ord + con : con
+let ordR = ord + '/r/';
+let fflateUrl = U + 'f815bd5c566c6e46de5cdb6ccb3a7043c63deeba61f4234baea84b602b0d4440i0';
+let p5Url = U + 'cc5cf94da24c1f6f0d435ccca78c24e98ca30adb1f3b7c81b9ab28ceb6cb628fi0'; // p5.js 1.9.0
+console.log("base url: ", U);
 
-function addSeed() {
+// thanks to the colorBlocks for this solution
 
-  dna = seed = W.location.pathname.split('/')[2] || W.location.href.split('/').find(t => t.includes('i0'))
+const gzlib = {
+  load: async (t, e, ev) => {
+    const a = await (await fetch(t)).text(),
+      c = new Blob([a], {
+        type: "application/javascript"
+      }),
+      n = URL.createObjectURL(c),
+      i = await import(n),
+      o = await (await fetch(e)).text(),
+      d = i.strFromU8(i.decompressSync(i.strToU8(atob(o), !0))),
+      p = D.createElement("script");
+    p.innerHTML = d, D.head.appendChild(p), D.dispatchEvent(new Event(ev))
+  }
+};
+
+let unzipLib = (zipLibUrl, e) => {
+  e = e || zipLibUrl
+  gzlib.load(fflateUrl, zipLibUrl, e);
+  W.addEventListener(e, () => {
+    console.log('--->', e);
+  })
+}
+
+
+// decompress
+
+let dFn = (compressId, loc) => {
+  compressId = loc? compressId: U + compressId
+  unzipLib(compressId)
+}
+
+
+
+let p5 = () => {
+
+  let s = D.getElementsByTagName('script');
+
+  for (let i = 0; i < s.length; i++) {
+    let currentScript = s[i];
+    let src = currentScript.getAttribute('src');
+
+    if (src && src.includes('?p5')) {
+      unzipLib(p5Url, 'p5Loaded');
+      console.log("** P5js helper - by The Function Gallery **");
+      break;
+    }
+  }
+}
+
+
+let addSeed= () => {
+
+  seed = W.location.href.split('/').find(t => t.includes('i0')) || W.location.pathname.split('/')[2]
 
   // static seed generator
   let stat = new URLSearchParams(W.location.search).get('static')
@@ -24,9 +86,8 @@ function addSeed() {
 
   if (seed == null || seed.length < 64) {
     const alphabet = "0123456789abcdefghijklmnopqrstuvwsyz"
-    seed = new URLSearchParams(window.location.search).get("seed") || Array(64).fill(0).map(_ => alphabet[(M.random() * alphabet.length) | 0]).join('') + "i0"
+    seed = new URLSearchParams(W.location.search).get("seed") || Array(64).fill(0).map(_ => alphabet[(M.random() * alphabet.length) | 0]).join('') + "i0"
   } else {
-    seed = seed.substring(0, seed.length - 2)
     console.log(seed);
     let pattern = "seed="
     for (let i = 0; i < seed.length - pattern.length; ++i) {
@@ -37,12 +98,12 @@ function addSeed() {
     }
   }
 
-
-
 }
 
 addSeed()
+p5()
 
+dna = seed.substring(0, seed.length - 2)
 
 function cyrb128($) {
   let _ = 1779033703,
@@ -56,7 +117,7 @@ function cyrb128($) {
 function sfc32($, _, u, i) {
   return function() {
     u >>>= 0, i >>>= 0;
-    var l = ($ >>>= 0) + (_ >>>= 0) | 0;
+    let l = ($ >>>= 0) + (_ >>>= 0) | 0;
     return $ = _ ^ _ >>> 9, _ = u + (u << 3) | 0, u = (u = u << 21 | u >>> 11) + (l = l + (i = i + 1 | 0) | 0) | 0, (l >>> 0) / 4294967296
   }
 }
@@ -69,8 +130,6 @@ class Rnd {
   I = (r = 0, t) => (t === undefined ? (t = r, r = 0) : undefined, 0 | this.N(r, t + 1));
   B = (r) => this.D() < r;
 }
-
-
 
 const R = new Rnd()
 const RI = R.I
@@ -85,7 +144,7 @@ let map = (v, n, m, n2, m2) => {
   return ((v - n) / (m - n) * (m2 - n2)) + n2
 }
 
-console.log("** Generative seed helper - by The Function Gallery **");
+console.log("** seed helper - by The Function Gallery **");
 
 
 // Long number seed random
@@ -110,53 +169,48 @@ let N = GLN(seed)
 let bin = BIN(N)
 
 // fire this to get recurring DNA updates
-// if u don't call getDNA, DNA will remain static as will seed long number random generator S()
+// if u don't call getDNA, DNA will remain static as will seed long number random pointer S()
 // will will then be a normal deterministic random generator
 
-// getDNA default update is 5 mins, not point in making it shorter
-// because blocks update every 10 min on ave.
+// getDNA default update is 5 mins, no point in making it shorter
+// because blocks update every 10 min on average.
 
 let getDNA = (t) => {
   t = t || 300
   blockhash = setDNA()
-  setInterval(setDNA, t * e3)
+  setInterval(setDNA, t * 10e3)
 }
 
+let dnaEndpoint = 'blockhash';
 
 // gets latest blockhash and seeds it into the DNA
 
 async function setDNA() {
   try {
-    const r = await getEndpoint('blockhash');
+    const r = await getEndpoint(dnaEndpoint);
 
     let d = JSON.parse(r);
-    // if (test && blockHash == r) {
-    //
-    // }
+
     const startIndex = Math.max(0, d.length - 16);
     const endIndex = Math.max(8, d.length);
 
     dna = d.slice(startIndex, endIndex) + dna.slice(8, d.length);
-    //console.log("DNA", dna);
 
-    // generate a new long number
     N = GLN(dna);
     return r;
   } catch (e) {
-    console.log('DNA fetch fail')
+    console.log('DNA fail')
     return blockHash;
     //throw e;
   }
 }
 
 
-// calls the blockhash endpoint
-// but can also be used as a helper function to call
-// any other Ordinal endpoints
+// helper function to call Ordinal endpoints
 
 function getEndpoint(url) {
-  let U = dev ? 'https://ordinals.com' : '';
-  U = U + "/r/" + url;
+
+  let U = ordR + url;
   return fetch(U)
     .then(response => {
       if (!response.ok) {
@@ -165,10 +219,9 @@ function getEndpoint(url) {
       return response.text();
     })
     .catch(error => {
-      console.error(error, "endpoint fetch fail");
+      console.error(error, "endpoint fail");
     });
 }
-
 
 
 
@@ -176,7 +229,7 @@ function getEndpoint(url) {
 
 function GLN(n) {
   let l = ""
-  for (var i = 0; i < n.length; i += 1) {
+  for (let i = 0; i < n.length; i += 1) {
 
     let h = getHash(hash32(n.substr(i, 2), 1))
     h = h.toString().substr(2, h.length)
@@ -196,7 +249,7 @@ function getHash(v) {
 }
 
 function hash32(str, v) {
-  var i, l,
+  let i, l,
     hval = 0x811c9dc5
   for (i = 0, l = str.length; i < l; i++) {
     hval ^= str.charCodeAt(i)
@@ -216,10 +269,10 @@ function BIN(n) {
 
   let out = ""
   for (let i = 0; i < n.length; i++) {
-    var e = n[i].charCodeAt(0)
-    var s = ""
+    let e = n[i].charCodeAt(0)
+    let s = ""
     do {
-      var a = e % 2
+      let a = e % 2
       e = (e - a) / 2
       s = a + s
     } while (e != 0)
@@ -242,60 +295,31 @@ let isBin = (n) => {
 let SR = (n, m, x, i) => RO(S(n, m, x, i))
 
 
-async function dFn(ordId) {
 
 
-    let U = dev ? 'https://ordinals.com/content/' : ''
-    let fflateUrl = U + '2dbdf9ebbec6be793fd16ae9b797c7cf968ab2427166aaf390b90b71778266abi0';
-
-async function fetchFlate(url) {
-    try {
-      let r = await fetch(url);
-      return (await r.text()).split("\n")[28];
-    } catch (e) {
-      console.log(`Failed fetch: ${e.message}`);
-      return null;
-    }
-  }
-
-
-  ffS = await fetchFlate(fflateUrl)
-
-  OI = await fetch(ordId);
-  OT = `${await OI.text()}`;
-
-  // deflate P5
-
-  (function () {
-    let ff = (new Function(`
-    return (function() {
-      ${ffS}
-      return { gunzipSync : sr, strFromU8 : ur };
-    })();
-  `))();
-    CC = ff.strFromU8(ff.gunzipSync(new Uint8Array(Array.from(atob(OT)).map((char) => char.charCodeAt(0)))));
-    CS = D.createElement('script');
-    CS.innerHTML = CC;
-    D.body.appendChild(CS);
-  })();
-
-
+async function getInfo(ordId){
+      let res = await fetch(ordR + 'inscription/' + ordId);
+      let info = await res.json();
+      return info;
 }
+
+
 //// CSS
 
 
-  const style = D.head.appendChild(document.createElement('style'))
+const style = D.head.appendChild(document.createElement('style'))
 
-  style.textContent = `
+style.textContent = `
 
   html, body {
+    height: 100%;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background: #000;
   }
 
   body {
-    height: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
